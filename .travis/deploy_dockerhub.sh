9e1e23c65353712bb728e5876166d1504d628089
Samuel <atulesamuel20@gmail.com>
2018-12-23 10:41:35 +0000

    .

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker login $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login --username $DOCKER_USER --password-stdin $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
