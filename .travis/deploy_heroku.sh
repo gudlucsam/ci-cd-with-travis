480d9f3cad5243f44d3b0f8cfbfef6acf893fb38
Samuel <atulesamuel20@gmail.com>
2018-12-23 17:15:00 +0000

    .

---
+++ .travis/deploy_heroku.sh
@@ -0,0 +1,4 @@
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install @heroku-cli/plugin-container-registry
+docker login --username _ --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:push web --app $HEROKU_APP_NAME
